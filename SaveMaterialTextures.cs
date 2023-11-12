b0bfe3e47da05abc02a2571f349c643d1d27a3c6
radsi <78619513+radsi@users.noreply.github.com>
2023-11-12 07:32:49 +0100

    Create SaveMaterialTextures.cs

---
+++ SaveMaterialTextures.cs
@@ -0,0 +1,45 @@
+using BepInEx;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+static public async Task SaveTexture2DToFileAsync(Texture2D tex, string filePath, SaveTextureFileFormat fileFormat, int jpgQuality) {
+  await Task.Run(() => {
+    switch (fileFormat) {
+    case SaveTextureFileFormat.JPG:
+      File.WriteAllBytes(filePath, tex.EncodeToJPG(jpgQuality));
+      break;
+    case SaveTextureFileFormat.PNG:
+      File.WriteAllBytes(filePath, tex.EncodeToPNG().ToArray());
+      break;
+    }
+  });
+}
+
+static public void SaveMaterialTexturesToFileAsync(Material material, SaveTextureFileFormat fileFormat = SaveTextureFileFormat.PNG, int jpgQuality = 95) {
+  string path = Path.Combine(Paths.PluginPath, "textures_dump", material.name);
+
+  string[] names = material.GetTexturePropertyNames();
+  foreach(string name in names) {
+    string filePath = Path.Combine(path, $ "{name}.png");
+    if (File.Exists(filePath)) return;
+
+    Texture currentTexture = material.GetTexture(name);
+
+    Texture2D texture2D = new Texture2D(currentTexture.width, currentTexture.height, TextureFormat.RGBAFloat, false);
+
+    RenderTexture currentRT = RenderTexture.active;
+
+    RenderTexture renderTexture = new RenderTexture(currentTexture.width, currentTexture.height, 32);
+    Graphics.Blit(currentTexture, renderTexture);
+
+    RenderTexture.active = renderTexture;
+    texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+    texture2D.Apply();
+
+    RenderTexture.active = currentRT;
+
+    Task.Run(async () => await SaveTexture2DToFileAsync(texture2D, filePath, fileFormat, jpgQuality));
+  }
+}
